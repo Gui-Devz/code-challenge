626d8a7332995efdb99a00322a05f7928daa1e33
Guilherme <guilherme.devz@hotmail.com>
2022-01-31 19:09:49 -0300

    feat: creating the API's for retrieving data from external API

---
+++ src/pages/api/brands.ts
@@ -0,0 +1,32 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import axios from "axios";
+import type { NextApiRequest, NextApiResponse } from "next";
+
+type Data = {
+  name: string;
+  code: string;
+  image: string;
+};
+
+interface Response {
+  data: Data[];
+}
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Response>
+) {
+  const result = axios
+    .get("https://bcpag-test-mock.azurewebsites.net/api/brands", {
+      headers: {
+        "X-Functions-Key":
+          "6Lap4DX36mVCdSMTRxpIMAvNzwMRIXrq1iW2CMO2gUzf8vzvvJEJaA==",
+      },
+    })
+    .then((response) => {
+      return res.status(200).json({ data: response.data });
+    })
+    .catch((err) => {
+      return res.send(err);
+    });
+}
---
+++ src/pages/api/simulate.ts
@@ -0,0 +1,41 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import axios from "axios";
+import type { NextApiRequest, NextApiResponse } from "next";
+
+type BrandSimulation = {
+  brand: string;
+  tax: number;
+  tax_amount: number;
+};
+
+type Data = {
+  amount: number;
+  brands: string[];
+  installment: number;
+  brand_simulation: BrandSimulation[];
+};
+
+interface Response {
+  data: Data[];
+}
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Response>
+) {
+  const body = req.body;
+
+  const data = axios
+    .post("https://bcpag-test-mock.azurewebsites.net/api/simulate", body, {
+      headers: {
+        "X-Functions-Key":
+          "6Lap4DX36mVCdSMTRxpIMAvNzwMRIXrq1iW2CMO2gUzf8vzvvJEJaA==",
+      },
+    })
+    .then((response) => {
+      return res.status(200).json({ data: response.data.data });
+    })
+    .catch((err) => {
+      return res.send(err);
+    });
+}
